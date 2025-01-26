4c9c39ba9b534cf07f9b85d03b2f6aaee600ea56
Oleksandr V <dev.ovoronkov@gmail.com>
2025-01-27 00:44:30 +0100

    chore(web-client): remove project

--- web-client/src/schemas/room-validation.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-export const createRoomSchema = z.object({
-	title: z.string().min(1).max(64)
-});
-
-export type CreateRoomSchemaType = z.infer<typeof createRoomSchema>;
--- web-client/src/types/user-types.ts
+++
@@ -1,5 +0,0 @@
-import type { LoginUserSchemaType, RegisterUserSchemaType } from '../schemas/user-validation';
-
-export interface RegisterUserRequestDto extends Omit<RegisterUserSchemaType, 'confirmPassword'> {}
-
-export interface LoginUserRequestDto extends LoginUserSchemaType {}
